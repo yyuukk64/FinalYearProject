3d9fdd05fd7c4e8435bb90045be767ad05990b81
yyuukk64 <114565477+yyuukk64@users.noreply.github.com>
2023-05-09 13:08:30 +0800

    Creating The Story in Forest1-4

--- FYP_URP/Assets/FYP/scripts/SaveSystem/TemporarilySaving/TemporarilySave.cs
+++ FYP_URP/Assets/FYP/scripts/SaveSystem/TemporarilySaving/TemporarilySave.cs
@@ -13,6 +13,11 @@ public class TemporarilySave : Singleton<TemporarilySave>
     public Vector3 posBeforeBattle;
     public Vector3 posForGate;
 
+    //process
+    public bool EnteredForest1_1;
+    public bool EnteredForest1_4;
+    public bool Passed_and_Get_Scissor;
+
     void Start()
     {
         DontDestroyOnLoad(this);
@@ -27,5 +32,9 @@ public class TemporarilySave : Singleton<TemporarilySave>
 
         sceneBeforeBattle = player.currentScene;
         //posBeforeBattle = player.transform.position;
+
+        EnteredForest1_1 = player.EnteredForest1_1;
+        EnteredForest1_4 = player.EnteredForest1_4;
+        Passed_and_Get_Scissor = player.Passed_and_Get_Scissor;
     }
 }
